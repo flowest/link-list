d5662302111472d6bf52533095694febcec94a26
Florian Westphal <f.westphal@hs-furtwangen.de>
2017-04-27 11:35:20 +0200

    added dummy implementation for firebase storage to save real img-files

--- src/app/dashboard-component/dashboard.component.ts
+++ src/app/dashboard-component/dashboard.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
+import { AngularFire, FirebaseListObservable, FirebaseApp } from 'angularfire2';
+
+import { Observable } from 'rxjs';
+
 
 @Component({
     moduleId: module.id,
@@ -7,10 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-    constructor() { }
+    firebaseApp: any;
 
-    ngOnInit() { 
+    imgUrl: string;
 
+    constructor( @Inject(FirebaseApp) firebaseApp: any) {
+        this.firebaseApp = firebaseApp
+    }
+
+    ngOnInit() {
+
+    }
+
+    uploadImage() {
+        // Create a root reference
+        let storageRef = this.firebaseApp.storage().ref();
+
+        let success = false;
+        // This currently only grabs item 0, TODO refactor it to grab them all
+        for (let selectedFile of [(<HTMLInputElement>document.getElementById('file')).files[0]]) {
+            //console.log(selectedFile);
+            // Make local copies of services because "this" will be clobbered
+            // let router = this.router;
+            // let af = this.af;
+            // let folder = this.folder;
+            // let path = `/${this.folder}/${selectedFile.name}`;
+            var iRef = storageRef.child("/img/jochen/" + selectedFile.name);
+            iRef.put(selectedFile).then((snapshot) => {
+                // console.log('Uploaded a blob or file! Now storing the reference at', `/${this.folder}/images/`);
+                // af.database.list(`/${folder}/images/`).push({ path: path, filename: selectedFile.name })
+                this.imgUrl = storageRef.child("/img/jochen/" + selectedFile.name).getDownloadURL().then(function (url){
+                    console.log(url);
+                });
+            });
+        }
     }
 
 }
